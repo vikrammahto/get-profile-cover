ddc4365195794209b0866ae627dbc4f1e39701f2
vikrammahto <vikramkr.in@gmail.com>
2025-04-29 20:17:51 +0530

    test commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Editor from '@/components/Editor';
+
 export default function Home() {
   return (
     <div className="flex h-dvh w-full items-center justify-center">
-      Hello world!
+      <Editor />
     </div>
   );
 }
---
+++ src/components/Editor.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Toggle } from '@/components/ui/toggle';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Card, CardContent } from '@/components/ui/card';
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
+const Editor = () => {
+  return (
+    <div>
+      Test
+    </div>
+  );
+};
+
+export default Editor;
